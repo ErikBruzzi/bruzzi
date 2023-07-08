819d7aa349c923752fe756d50448e77e19facc67
ErikBruzzi <90414638+ErikBruzzi@users.noreply.github.com>
2023-07-07 22:26:43 -0300

    Add files via upload

---
+++ bruzzi/script.js
@@ -0,0 +1,31 @@
+
+/* const header = document.querySelector('header');
+let scrollPosition = 0;
+
+window.addEventListener('scroll', function() {
+    if (scrollPosition > 100) {
+        header.style.top = '-100px';
+    }else{
+        header.style.top = '0';
+    }
+})
+*/
+
+/*
+const startNow = document.getElementById("btn-menu")
+
+startNow.addEventListener("click", fnStart)
+
+function fnStart(){
+    document.documentElement.scrollTop = 0;
+}
+*/
+
+ //const input = document.getElementById("input-submit-container");
+/*
+ var nome = document.getElementsByName("nome");
+
+input = addEventListener("click", () => {
+    console.log(nome)
+}) 
+*/
